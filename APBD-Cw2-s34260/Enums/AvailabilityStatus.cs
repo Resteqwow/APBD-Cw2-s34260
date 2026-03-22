568d7f2fdc91c32c0180f54a32a5e76505fd5e51
Kamil Pawlak <s34260@pjwstk.edu.pl>
2026-03-22 14:12:11 +0100

    added basic equipment class

---
+++ APBD-Cw2-s34260/Enums/AvailabilityStatus.cs
@@ -0,0 +1,7 @@
+namespace APBD_Cw2_s34260.Enums;
+
+public enum AvailabilityStatus
+{
+    Available,
+    Unavailable,
+}
---
+++ APBD-Cw2-s34260/Models/Equipment.cs
@@ -0,0 +1,15 @@
+using APBD_Cw2_s34260.Enums;
+
+namespace APBD_Cw2_s34260.Models;
+
+public abstract class Equipment(string name)
+{
+    
+    private static int _nextId = 1;
+    
+    public int Id { get; set; } =  _nextId++;
+    public string Name { get; set; } = name;
+            
+    public AvailabilityStatus Availability { get; set; } = AvailabilityStatus.Available;
+    
+}
